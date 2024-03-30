eed9ef8e56ebe9e6da8642ac06c457cf44ad3d33
RinataAymaletdinova <125034890+RinataAymaletdinova@users.noreply.github.com>
2024-03-30 11:55:48 +0300

    Add files via upload

---
+++ kluch.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="kluch" tilewidth="40" tileheight="40" tilecount="2" columns="2">
+ <image source="kluch.png" width="82" height="73"/>
+</tileset>
